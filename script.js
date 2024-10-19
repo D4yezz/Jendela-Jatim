a3431aad4b899984ae95d3b81b588e37645c150b
D4yezz <adiasmuhsin1206@gmail.com>
2024-10-19 16:02:29 +0700

    Fix Bug

--- script.js
+++ script.js
@@ -56,12 +56,60 @@ function showSlider(type) {
   }, timeAutoNext);
 }
 
-document.querySelectorAll('.offcanvas .nav-link .btn-close').forEach(link => {
-  link.addEventListener('click', function() {
-    e.preventDefault();
-    const offcanvasElement = document.querySelector('#navbarOffcanvasLg');
-    const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
-    offcanvasInstance.hide();
-  });
-});
+function toggleMenu() {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("show");
+}
 
+// let lastScrollTop = 0;
+// const navbar = document.getElementById('navbar');
+
+// window.addEventListener('scroll', function() {
+//   const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+//   if (scrollTop > lastScrollTop) {
+//     // Scrolling down
+//     navbar.style.top = '-80px';  // Hide the navbar (adjust the value based on navbar height)
+//   } else {
+//     // Scrolling up
+//     navbar.style.top = '0';
+//   }
+
+//   lastScrollTop = scrollTop;
+// });
+
+// document.querySelectorAll(".offcanvas .nav-link .btn-close").forEach((link) => {
+//   link.addEventListener("click", function () {
+//     e.preventDefault();
+//     const offcanvasElement = document.querySelector("#navbarOffcanvasLg");
+//     const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
+//     offcanvasInstance.hide();
+//   });
+// });
+
+// document.querySelectorAll(".offcanvas .nav-link").forEach((link) => {
+//   link.addEventListener("click", function () {
+//     const offcanvasElement = document.querySelector("#navbarOffcanvasLg");
+//     const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
+//     if (offcanvasInstance) {
+//       offcanvasInstance.hide();
+//     }
+//   });
+// });
+
+// document.querySelector(".btn-close").addEventListener("click", function () {
+//   const offcanvasElement = document.querySelector("#navbarOffcanvasLg");
+//   const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
+//   if (offcanvasInstance) {
+//     offcanvasInstance.hide();
+//   }
+// });
+
+// document.addEventListener("click", function (event) {
+//   var navbar = document.querySelector(".navbar-collapse");
+//   if (navbar.classList.contains("show") && event.target.closest(".nav-link")) {
+//     // Tutup navbar setelah item dipilih
+//     var bootstrapNavbar = bootstrap.Collapse.getInstance(navbar);
+//     bootstrapNavbar.hide();
+//   }
+// });
